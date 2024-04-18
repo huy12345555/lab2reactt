d977d4c1ca2e3d634b44d82c5d491ac7ac4e978f
huy12345555 <hoanghuytmg@gmail.com>
2024-04-18 12:30:50 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+import { PaperProvider } from 'react-native-paper'
+import Contacts from './src/components/Contacts';
+import Routes  from './src/components/Routes';
+import Store from './src/components/Store';
+import { Provider } from 'react-redux';
+export default function App(){
+    return(
+   <Routes/>
+    )
+} 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
+    justifyContent: 'center',
+    marginHorizontal: 16,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
